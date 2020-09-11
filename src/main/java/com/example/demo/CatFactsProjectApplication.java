b02a1128daa9427d371c8c617d501b032f23f04b
PatrickVincentHoestroem <patrick.vincent@live.dk>
2020-09-11 11:32:48 +0200

    Initial commit

---
+++ src/main/java/com/example/demo/CatFactsProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatFactsProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatFactsProjectApplication.class, args);
+    }
+
+}
